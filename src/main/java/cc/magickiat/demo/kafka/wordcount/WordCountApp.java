240a317ee58e85cdfd0108ccc7aa94403ac7f587
Kiattikhun Prathumma <magicalcyber@gmail.com>
2020-05-06 22:58:12 +0700

    Init Commit

---
+++ src/main/java/cc/magickiat/demo/kafka/wordcount/WordCountApp.java
@@ -0,0 +1,21 @@
+package cc.magickiat.demo.kafka.wordcount;
+
+import org.apache.kafka.common.serialization.Serde;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.StreamsConfig;
+
+import java.util.Properties;
+
+public class WordCountApp {
+
+    public static void main(String[] args) {
+        // config to connect kafka
+        Properties config = new Properties();
+        config.put(StreamsConfig.APPLICATION_ID_CONFIG, "word-count-app");
+        config.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        config.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        config.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.Long().getClass());
+
+
+    }
+}
